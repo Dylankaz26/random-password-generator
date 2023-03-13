7d3922805ba90a4b6f2510a946f0734af9fb5995
dylan kazunaga <dylankkazunaga@yahoo.com>
2023-03-13 13:26:23 -0700

    defined some variables

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword(){
   //To be completed
   console.log("in generatePassword function")
+  let lower="abcdefghijklmnopqrstuvwxyz";
+  let upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let num="0123456789";
+  let special="!@#$%^&*()|{}/";
+  let pLen=0;
+  let newPsw="";
+  let validChars="";
+
+  
 }
 // Write password to the #password input
 function writePassword() {
